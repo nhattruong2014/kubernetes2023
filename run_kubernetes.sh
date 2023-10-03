09b83f59b1e6f11670db5414b98b09c4f4dbd68a
Truong Nguyen <nhattruong.ngn@gmail.com>
2023-10-03 22:22:10 +0700

    add kube

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,7 +8,7 @@ dockerpath="nhattruongngn/capstone:v1.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run microproject --image=nhattruongngn/capstone:v1.0 --port=80 --labels app=microproject
+kubectl run my-app-deployment-77bcd8856-bzdzx --image=nhattruongngn/capstone:v1.0 --port=80 --labels app=my-app
 
 # Step 3:
 # List kubernetes pods
